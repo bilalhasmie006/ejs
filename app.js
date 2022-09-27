8f2e853f0a9be6b5529902498fb4dfccec135678
bilalhasmie006 <111913856+bilalhasmie006@users.noreply.github.com>
2022-09-27 11:27:34 +0500

    list functionallity completed

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const https = require("node:https");
 const { monitorEventLoopDelay } = require("node:perf_hooks");
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+var items = [];
 
 app.set('view engine', 'ejs');//for setup the ejs engine
 
@@ -13,59 +15,26 @@ app.set('view engine', 'ejs');//for setup the ejs engine
 
 app.get('/', (req, res) => {
 
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var today  = new Date();
+    var result=today.toLocaleDateString("en-US",options); 
+console.log(today.toLocaleDateString("en-US",options));
 
-    var today = new Date("September 8, 2000 23:15:30");
-var day = today.getDay();
-var result
-//console.log(day);//ceck out if this works without the function or not.
-//if statements 
-{
-if(day===1)
-{
-    result="monday";
-}
-if(day===2)
-{
-    result="tuesday";
-}
-if(day===3)
-{
-    result="wednesday";
-}
-if(day===4)
-{
-    result="thursday";
-}
-if(day===5)
-{
-    result="friday";
-}
-if(day===6)
-{
-    result="saturday";
-}
-if(day===7)
-{
-    result="sunday";
-}
 
-} 
-    res.render('index', {html_day: result});//it will render the page name index
+
+
+
+res.render("index", {html_day: result,html_items: items});
   });
   
+  app.post("/",function(req,res){
 
+var item = req.body.newItem;
+items.push(item);
+res.redirect("/");
 
-//app.get("/",(req,res)=>{
+  })
 
-    //for checking day of certain date
-// var today = new Date("March 15, 1965 23:15:30");
-// var day = today.getDay();
-// console.log(day);
-
-   
-
-
-//});
 
 
     app.listen(process.env.PORT || 3000,()=>{
