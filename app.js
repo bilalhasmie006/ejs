0c24db0caf05e859e449f090cb80a0fdec0e4519
bilalhasmie006 <111913856+bilalhasmie006@users.noreply.github.com>
2022-10-08 12:27:04 +0500

    css is added .Now making webpage dynamic and creating templetes

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ let ejs = require('ejs');
 const bodyParser = require('body-parser');
 const https = require("node:https");
 const { monitorEventLoopDelay } = require("node:perf_hooks");
+const exp = require("node:constants");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+
 var items = [];
 
 app.set('view engine', 'ejs');//for setup the ejs engine
-
-
+app.use(express.static("public"));
    
 
 
@@ -19,12 +20,7 @@ app.get('/', (req, res) => {
     var today  = new Date();
     var result=today.toLocaleDateString("en-US",options); 
 console.log(today.toLocaleDateString("en-US",options));
-
-
-
-
-
-res.render("index", {html_day: result,html_items: items});
+    res.render("index", {html_day: result,html_items: items});
   });
   
   app.post("/",function(req,res){
@@ -36,9 +32,7 @@ res.redirect("/");
   })
 
 
-
-    app.listen(process.env.PORT || 3000,()=>{
-
-        console.log("server is running")
+  app.listen(process.env.PORT || 3000,()=>{
+ console.log("server is running")
         
         })
